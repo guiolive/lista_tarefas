78ce72e4334c8106860e2408c731b19b3a26e067
“oliguitos” <202501273653@alunos.estacio.br>
2025-10-12 11:30:44 -0300

    melhorias no css

--- lista.js
+++ lista.js
@@ -1,63 +1,69 @@
 let tarefas = [];
 
+window.onload = function () {
+  carregarTarefas();
+}; 
+
 function Adicionar() {
   const input = document.getElementById("inputTarefa");
   const textoTarefa = input.value;
 
   if (textoTarefa === "") {
-    alert("Tarefa vazia, digite uma tarefa");
+    alert("Tarefeu a vazia");
     return;
   }
 
   tarefas.push(textoTarefa);
-
   input.value = "";
 
-  mostrarTarefas();
   salvarTarefas();
-}
-
-function mostrarTarefas() {
-  const lista = document.getElementById("lista");
-  lista.innerHTML = "";
-
-  tarefas.forEach((tarefa, indice) => {
-    const li = document.createElement("li");
-    li.textContent = tarefa + " ";
-    const botaoRemover = document.createElement("button");
-    botaoRemover.textContent = "X";
-
-    botaoRemover.onclick = function () {
-      removerTarefa(indice);
-    };
-
-    li.appendChild(botaoRemover);
-    lista.appendChild(li);
-  });
-
-  const contador = document.getElementById("contador");
-  const numeroTarefas = tarefas.length;
-  contador.textContent = `Voce tem ${numeroTarefas} tarefas`;
-}
-
-function removerTarefa(indice) {
-  tarefas.splice(indice, 1);
   mostrarTarefas();
-  salvarTarefas();
 }
 
 function salvarTarefas() {
   localStorage.setItem("tarefas", JSON.stringify(tarefas));
 }
 
-function carregarTarefas() {
-  const tarefasString = localStorage.getItem("tarefas");
-  if (tarefasString) {
-    tarefas = JSON.parse(tarefasString);
-    mostrarTarefas();
+function mostrarTarefas() {
+  // Parte 1: Pegar o elemento e depois limpar
+  const lista = document.getElementById("lista");
+  lista.innerHTML = "";
+
+  // Loop que cria cada item
+  tarefas.forEach((tarefa, indice) => {
+    // Cria <li>
+    const li = document.createElement("li");
+    li.textContent = tarefa + " ";
+
+    // Cria botao
+    const botaoRemover = document.createElement("button");
+    botaoRemover.textContent = "X";
+
+    //Define o que o botão faz
+    botaoRemover.onclick = function () {
+      removerTarefa(indice);
+    };
+
+    //Adiciona botao no <li>
+    li.appendChild(botaoRemover);
+
+    //Adiciona <li> na lista
+    lista.appendChild(li);
+  });
+
+  const contador = document.getElementById("contador");
+  const numeroTarefas = tarefas.length;
+  let palavra;
+  if (numeroTarefas === 1) {
+    palavra = "tarefa";
+  } else {
+    palavra = "tarefas";
   }
 }
 
-window.onload = function () {
-  carregarTarefas();
-};
+function removerTarefa(indice) {
+  tarefas.splice(indice, 1);
+
+  salvarTarefas();
+  mostrarTarefas();
+}
